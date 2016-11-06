048800a215918eee4249fe8ff535e4f65c094897
Eric Fredine <eric.fredine@gmail.com>
2016-11-06 14:06:42 +0000

    Explicitly provide the encoding and mode.

--- file-io-exercise/read-file.js
+++ file-io-exercise/read-file.js
@@ -1,11 +1,11 @@
 var fs = require('fs');
 var fileName = process.argv[2];
 if (fileName) {
-  fs.readFile(fileName, (err, data) => {
+  fs.readFile(fileName, {encoding: 'utf8', mode: 'r'}, (err, data) => {
     if (err) {
       console.error(err);
     } else {
-      console.log(data.toString());
+      console.log(data);
     }
   });
 }
